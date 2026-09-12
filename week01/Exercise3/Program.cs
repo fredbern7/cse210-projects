da45468531b3215e87dd82e27aa9ff2dae98eff3
fredbern7 <fredbern7@gmail.com>
2026-09-13 10:17:12 +1100

    first update to exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,39 @@
 using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        // Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string play = "";
+
+        while (play != "no")
+        {
+            Random randomGenerator = new Random();
+            int MagicNumber = randomGenerator.Next(1, 100);
+
+            int guess = -1;
+            while (guess != MagicNumber)
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                Console.WriteLine(MagicNumber);
+
+                if (MagicNumber > guess)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (MagicNumber < guess)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+            }
+    
+            Console.WriteLine("Still wanna play? yes/no: ");
+            play = Console.ReadLine();
+        }
     }
 }
